fa2c4b7995ca3f85dd050976a6728ce3eafbd0ef
haohe@unicloud.com <hao503106@163.com>
2021-07-01 11:01:43 +0800

    fix:修改报错

--- src/router/index.js
+++ src/router/index.js
@@ -42,16 +42,16 @@ export const routes = [
       },
     ],
   },
-  // {
-  //   // login
-  //   path: '/login',
-  //   name: 'login',
-  //   meta: { title: 'login', category: 'login', parent: '-1' },
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () => import(/* webpackChunkName: "about" */ '../views/login.vue'),
-  // },
+  {
+    // login
+    path: '/login',
+    name: 'login',
+    meta: { title: 'login', category: 'login', parent: '-1' },
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/login.vue'),
+  },
 ];
 
 const router = new VueRouter({
